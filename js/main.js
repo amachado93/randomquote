31bafb9b348cf8c3e8eb33ea2d64943559a02586
Alex Machado <amachado@sva.edu>
2018-01-29 16:30:40 -0500

    replace quotes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 (function() {
   var quotes = [
-  'The slimy old lady proudly dodged because some dog passionately killed towards a soft boy which, became a soft, lovely teacher. - Walt Disney',
-  'The slimy plastic precisely sliced because some dog passionately killed towards a lovely professor which, became a professional, dumb duck. - FDR',
-  'The slimy duck passionately kicked because some dog passionately killed towards a dumb plastic which, became a slimy, beautiful bird. - Me',
-  'The soft clock humbly ran because some old lady proudly died above a lazy boy which, became a rough, lazy clock. - Oscar Wilde',
-  'The lazy duck calmly flew because some clock elegantly ran into a dumb hamster which, became a vibrating, professional bird. - Rowan Atkinson',
-  'The rough professor quickly dodged because some teacher humbly rolled below a hot plastic which, became a lovely, lovely professor. - Einstein',
+  'But man is not made for defeat.  A man can be destroyed but not defeated - Hemingway',
+  'When you reach the end of your rope, tie a knot and hang on. - FDR',
+  'Nothing is permanent except change. - Heraclitus',
+  'You cannot shake hands with a clenched fist. - Gandhi',
+  'There is no charm equal to tenderness of heart. - Jane Austen',
+  'Learning never exhausts the mind. - da Vinci',
   'When you think about an apple, you also think about the opposite of an apple. - Jaden Smith',
   'ayy lmao - unknown',
-  'Welcome to Hollywood, baby! You got star material written all over you! - Audrey Hepburn',
-  'I was playing Minecraft on survival single player and herobrain hacked me and took away all my diamonds and killed me!1! - Typical Minecraft Player'
+  'All that we see or seem is but a dream within a dream. - Edgar Allan Poe',
+  'Good judgement comes from experience, and a lot of that comes from bad judgement - Will Rodgers'
 ]
 
   var button = document.getElementsByTagName('button')
